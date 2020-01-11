5848789b6dae8dda41433a753aee53ee49a36e20
Claire-web <57494370+Claire-web@users.noreply.github.com>
2020-01-11 21:42:54 +0800

    Update driving.py

--- driving.py
+++ driving.py
@@ -1,15 +1,16 @@
 # 國家年齡考照問題
 country = input('請輸入所在國家: ')
-age = input('請輸入年齡: ')
 if country == '台灣':
-	if int(age) >= 18:
-		print('你可以考駕照')
-	else:
-		print('你還不能考駕照')
+    age = input('請輸入年齡: ')
+    if int(age) >= 18 :
+        return '你可以考駕照'
+    else:
+        return '你還不能考駕照'
 elif country == '美國':
-	if int(age) >= 16:
-		print('你可以考駕照')
-	else:
-		print('你還不能考駕照')
-else: 
-	print('請輸入 台灣/美國')
+    age = input('請輸入年齡: ')
+    if int(age) >= 16 :
+        print('你可以考駕照')
+    else:
+        print('你還不能考駕照')
+else:
+    print('請輸入 台灣/美國')
